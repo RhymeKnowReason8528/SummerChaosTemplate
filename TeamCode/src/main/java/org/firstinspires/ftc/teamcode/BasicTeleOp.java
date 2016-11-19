946da8dc714bdf4ec0baf93949e74ca2d4d22be0
DrSuess2 <truccmom@yahoo.com>
2016-11-18 20:35:36 -0500

    added launcher functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
 /**
  * Created by RobotK on 10/27/2016.
@@ -11,7 +10,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 @TeleOp (name = "MecanumTeleOp")
 public class BasicTeleOp extends OpMode{
 
-   private Robot myRobot = null;
+    private Robot myRobot = null;
 
     @Override
     public void init() {
@@ -23,10 +22,10 @@ public class BasicTeleOp extends OpMode{
         x1 = scaleInput(x1);
         y1 = scaleInput(y1);
 
-        double FL = y1 + x1 + x2;
-        double FR = y1 - x1 - x2;
-        double BL = y1 - x1 + x2;
-        double BR = y1 + x1 - x2;
+        double FL = y1 - x1 - x2;
+        double FR = y1 + x1 + x2;
+        double BL = y1 + x1 - x2;
+        double BR = y1 - x1 + x2;
 
         double mv = max(Math.abs(FL), Math.abs(FR), Math.abs(BL), Math.abs(BR));
         if (Math.abs(mv) > 1) {
@@ -56,6 +55,32 @@ public class BasicTeleOp extends OpMode{
     @Override
     public void loop() {
         mecanumDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+
+        if(gamepad1.right_bumper) {
+            myRobot.collectorMotor.setPower(-1);
+        } else {
+            myRobot.collectorMotor.setPower(0);
+        }
+
+        if(gamepad1.b) {
+            myRobot.launcherMotor.setPower(1);
+        } else {
+            myRobot.launcherMotor.setPower(0);
+        }
+
+        if(gamepad1.left_bumper) {
+            myRobot.engageLauncher();
+        } else if(gamepad1.left_trigger > 0) {
+            myRobot.disengageLauncher();
+        }
+
+        if(gamepad1.x) {
+            myRobot.lockLauncher(this);
+        } else if (gamepad1.y) {
+            myRobot.disengageLauncher();
+        }
+        telemetry.addData("Servo position", myRobot.launcherServo.getPosition());
+        telemetry.update();
     }
 
     private double scaleInput(double dVal) {

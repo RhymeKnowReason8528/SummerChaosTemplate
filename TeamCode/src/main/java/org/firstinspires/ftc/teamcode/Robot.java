620a2193d564ba24aadf16e7472510827e8ed9e9
DrSuess2 <truccmom@yahoo.com>
2016-11-18 20:36:04 -0500

    robot class catchup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.text.method.Touch;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
@@ -26,16 +30,24 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 public class Robot {
     /* Public OpMode members. */
-    public DcMotor  leftFrontMotor  = null;
-    public DcMotor  leftRearMotor  = null;
+    public DcMotor  leftFrontMotor   = null;
+    public DcMotor  leftRearMotor    = null;
     public DcMotor  rightFrontMotor  = null;
-    public DcMotor  rightRearMotor  = null;
+    public DcMotor  rightRearMotor   = null;
 
-    //public DcMotor  launcherMotor  = null;
+    public DcMotor collectorMotor    = null;
+
+    public DcMotor  launcherMotor    = null;
+    public Servo    launcherServo;
+
+    public TouchSensor launcherLimitTouchSensor;
 
     /* Local OpMode members. */
     private HardwareMap hwMap  = null;
     private ElapsedTime period  = new ElapsedTime();
+
+    double encoderSubtractor;
+    double launcherTimeout;
 
     /* Constructor */
     public Robot() {
@@ -54,11 +66,22 @@ public class Robot {
         leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
         leftRearMotor.setDirection(DcMotor.Direction.REVERSE);
 
+        collectorMotor = hwMap.dcMotor.get("collector");
+
+        launcherMotor = hwMap.dcMotor.get("launcher_motor");
+        launcherServo = hwMap.servo.get("launcher_servo");
+
+        launcherLimitTouchSensor = hwMap.touchSensor.get("launcher_limit_sensor");
+
+
         // Set all motors to zero power
         leftFrontMotor.setPower(0);
         leftRearMotor.setPower(0);
         rightFrontMotor.setPower(0);
         rightRearMotor.setPower(0);
+        collectorMotor.setPower(0);
+        launcherMotor.setPower(0);
+        launcherServo.setPosition(0.5);
 
         // Set all motors to run without encoders.
         // May want to use RUN_USING_ENCODERS if encoders are installed.
@@ -66,7 +89,8 @@ public class Robot {
         leftRearMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightFrontMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightRearMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        collectorMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        launcherMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         // Define and initialize ALL installed servos.
     }
 
@@ -93,5 +117,54 @@ public class Robot {
 
         // Reset the cycle clock for the next pass.
         period.reset();
+    }
+
+    public void moveForward(DcMotor leftFrontMotor, DcMotor leftRearMotor, DcMotor rightFrontMotor, DcMotor rightRearMotor, double distance, double speed, boolean active) {
+        encoderSubtractor = leftFrontMotor.getCurrentPosition();
+        if(distance >= 0) {
+            while(leftFrontMotor.getCurrentPosition() - encoderSubtractor < distance && active == true) {
+                leftFrontMotor.setPower(speed);
+                leftRearMotor.setPower(speed);
+                rightFrontMotor.setPower(speed);
+                rightRearMotor.setPower(speed);
+            }
+            leftFrontMotor.setPower(0);
+            leftRearMotor.setPower(0);
+            rightFrontMotor.setPower(0);
+            rightRearMotor.setPower(0);
+        } else {
+            while(leftFrontMotor.getCurrentPosition() - encoderSubtractor > distance && active == true) {
+                leftFrontMotor.setPower(-speed);
+                leftRearMotor.setPower(-speed);
+                rightFrontMotor.setPower(-speed);
+                rightRearMotor.setPower(-speed);
+            }
+            leftFrontMotor.setPower(0);
+            leftRearMotor.setPower(0);
+            rightFrontMotor.setPower(0);
+            rightRearMotor.setPower(0);
+        }
+    }
+
+    public void engageLauncher() {
+        launcherServo.setPosition(0.1);
+    }
+
+    public void disengageLauncher() {
+        launcherServo.setPosition(1);
+    }
+
+    public void lockLauncher(OpMode opmode) {
+        engageLauncher();
+        waitForTick(500);
+
+        launcherTimeout = opmode.getRuntime();
+
+        while(!launcherLimitTouchSensor.isPressed() && opmode.getRuntime() < launcherTimeout + 5) {
+           // launcherMotor.setPower(1);
+            opmode.telemetry.addData("status", "Pulling back");
+        }
+        launcherMotor.setPower(0);
+        opmode.telemetry.addData("status", "Launcher ready to fire");
     }
 }
